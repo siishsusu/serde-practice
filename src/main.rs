84b4610c472bb274fa753e9d26dcf1402623ccbd
siishsusu <byvladyslava@gmail.com>
2024-11-18 17:22:52 +0200

    Testing

--- src/main.rs
+++ src/main.rs
@@ -64,6 +64,25 @@ struct Request {
     debug: Debug,
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn test() {
+        let mut file = File::open("request.json").unwrap();
+        let mut json_str = String::new();
+        file.read_to_string(&mut json_str).unwrap();
+
+        let request: Request = serde_json::from_str(&json_str).unwrap();
+        assert_eq!(request.stream.public_tariff.id, 1);
+        assert_eq!(request.stream.private_tariff.client_price, 250);
+        assert_eq!(request.gifts.len(), 2);
+        assert_eq!(request.gifts[0].description, "Gift 1");
+    }
+}
+
 fn main() {
     let mut file = File::open("request.json").unwrap();
     let mut json_str = String::new();
